21023bc358ab0e8c09206e17e1303748964f3009
Ben Cifu <ben.cifu@yale.edu>
2023-02-03 16:11:51 -0500

    Can now print out function list

--- main.cpp
+++ main.cpp
@@ -9,6 +9,15 @@
 using namespace antlr4;
 using namespace std;
 
+string listToString(list<string> lst){
+    string result;
+    for(const auto& s : lst){
+        result += s;
+        result += " ";
+    }
+    return result;
+}
+
 class HullTreeShapeListener : public HullQueryBaseListener {
     public:
         unordered_map<std::string, list<string>> varaible_map; 
@@ -20,7 +29,7 @@ class HullTreeShapeListener : public HullQueryBaseListener {
         if(decl == nullptr) return; //not a declaration, don't process here.
 
         //toStringTree or getText on terminal
-        std::cout << "Decl: " << decl->ID()->toStringTree() << std::endl;
+        // std::cout << "Decl: " << decl->ID()->toStringTree() << std::endl;
         string variable_name = decl->ID()->getText();
         //need to handle if several variables assigned (a = b = c.func()), maybe do with other expressions, just assign the right
 
@@ -37,10 +46,16 @@ class HullTreeShapeListener : public HullQueryBaseListener {
             this->varaible_map[variable_name].push_back(right->var()->getText());
         }
 
-        std::for_each(varaible_map[variable_name].begin(), varaible_map[variable_name].end(), [](const auto &e) {
-            std::cout << e << " ";
-        });
-        std::cout << std::endl;
+        HullQueryParser::FuncContext* func = right->func();
+        while(func != nullptr) { //while there are functions to work with
+            this->varaible_map[variable_name].push_back(func->ID()->getText());
+            //What is the best way to handle function arguments
+            func = func->func();
+        }
+        // std::for_each(varaible_map[variable_name].begin(), varaible_map[variable_name].end(), [](const auto &e) {
+        //     std::cout << e << " ";
+        // });
+        // std::cout << std::endl;
     }
 };
 
@@ -69,6 +84,10 @@ int main(int argc, char *argv[])
     HullTreeShapeListener listener;
 
     tree::ParseTreeWalker::DEFAULT.walk(&listener, tree);
+
+    for (auto iter = cbegin(listener.varaible_map); iter != cend(listener.varaible_map); ++iter){
+        cout << iter->first << ": " << listToString(iter->second) << endl; 
+    }
 
     return 0;
 }
